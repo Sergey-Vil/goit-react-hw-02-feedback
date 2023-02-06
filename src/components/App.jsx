fc64e1053cef50d68be6759ba16ada5b62e3edd8
Sergey-Vil <vilchinskiy.s.st@gmail.com>
2023-02-06 21:14:08 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,8 @@ export const App = () => {
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        // alignItems: 'center',
-        fontSize: 40,
+        // // alignItems: 'center',
+        // fontSize: 40,
         color: '#010101',
       }}
     >
